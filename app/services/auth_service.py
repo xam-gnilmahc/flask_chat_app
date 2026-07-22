98e7894393bc1f2531fb9aafcadd94c52f3db65e
max98703 <max.c@shikhartech.com>
2026-07-22 16:23:56 +0545

    updates on login auth part

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -33,6 +33,8 @@ class AuthService:
         user = UserService.get_by_username(username)
         if user is None or not check_password_hash(user["password_hash"], password):
             raise AuthError("Invalid username or password", 401)
+        if user.get("status", 1) != 1:
+            raise AuthError("Account is inactive. Contact support.", 403)
         return user
 
     @staticmethod
